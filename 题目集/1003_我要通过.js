f69d4d6f39da2f0fc772875a1e4f3d0006cc48ec
AAM0903 <54193936+AAM0903@users.noreply.github.com>
2019-12-20 16:57:44 +0800

    Update and rename 1003_我要通过.md to 题目集/1003_我要通过.js

---
+++ 题目集/1003_我要通过.js
@@ -0,0 +1,23 @@
+
+
+#include<stdio.h>
+int main()
+{
+	int sum = 0;
+	char c, *pinyin[] = {"ling", "yi", "er", "san", "si",
+						 "wu", "liu", "qi", "ba","jiu"};
+	
+	while ((c = getchar()) != '\n')
+		sum += c - '0';
+		
+	if(sum / 100)
+		printf("%s ", pinyin[sum / 100]);
+	if(sum / 10)
+		printf("%s ", pinyin[sum / 10 % 10]);
+	
+	printf("%s", pinyin[sum % 10]);
+	
+	return 0;
+}
+
+
